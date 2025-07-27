d13b860192d9d15c1be5e173a45ee281f2f48bea
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-07-27 16:57:32 +0300

    feat(auth): switch to Django’s built-in LoginView and LogoutView
    - Replace custom login_view and logout_view with Django’s built-in class-based views
    - Use LoginView and LogoutView with custom templates for authentication
    - Simplify authentication URL patterns and improve maintainability

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, register_view, login_view, logout_view
-from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import list_books, LibraryDetailView, register_view
 
 urlpatterns = [
-    path('books/', list_books, name='list_books'),  # FBV
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'), 
-    path('', views.list_books, name='list_books'), # CBV
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('', list_books, name='list_books'),
     path('register/', register_view, name='register'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
