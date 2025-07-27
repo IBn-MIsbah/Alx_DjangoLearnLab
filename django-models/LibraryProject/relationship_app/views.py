75200aefc3b1f4b057c3b83b22758678e69b3822
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-07-27 16:54:49 +0300

    Add user authentication: register, login, logout with templates and routing

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -16,6 +16,11 @@ from django.shortcuts import render
 from .models import Library, Book
 from django.views.generic.detail import DetailView
 
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
@@ -25,3 +30,30 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+# ? auth
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # log in user after registration
+            return redirect('list_books')  # adjust as needed
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('list_books')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'relationship_app/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
