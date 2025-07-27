7a3fc665dbd59f7c9723fb6e0d535d58501e5ee1
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-07-27 15:06:11 +0300

    restructure file

---
+++ django-model/LibraryProject/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,24 @@
+import os
+import django
+
+# Setup Django environment
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
+django.setup()
+
+from relationship_app.models import Author, Book, Library, Librarian
+
+# 1. Query all books by a specific author
+author_name = "Chinua Achebe"
+author = Author.objects.get(name=author_name)
+books_by_author = Book.objects.filter(author=author)
+print(f"Books by {author.name}: {[book.title for book in books_by_author]}")
+
+# 2. List all books in a library
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
+books_in_library = library.books.all()
+print(f"Books in {library.name}: {[book.title for book in books_in_library]}")
+
+# 3. Retrieve the librarian for a library
+librarian = Librarian.objects.get(library=library)
+print(f"Librarian for {library.name} is {librarian.name}")
