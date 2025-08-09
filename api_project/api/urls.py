8cd8296b3471572cb9cd3abd4f5161a0672bdb5f
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-08-09 10:23:52 +0300

    feat(api): add /books/ route mapped to BookList view

---
+++ api_project/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookList
+
+# router = DefaultRouter()
+# router.register(r'books', BookViewSet)
+
+urlpatterns = [
+   path('books/', BookList.as_view(), name='book-list')
+]
