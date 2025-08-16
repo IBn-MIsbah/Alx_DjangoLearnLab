5709705ba716cbd6f5e24878c302e67de600a751
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-08-16 23:04:30 +0300

    feat(auth): add user registration, login, logout, and profile views

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth import login
 
-# Create your views here.
+from .forms import UserRegisterationForm, UserUpdateForm, ProfileForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Welcome! Your account has been created.")
+
+            return redirect('profile')
+        else:
+            form = UserRegisterationForm()
+        return render(request, 'blog/register.html', {'form': form})
+    
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, "Profile updated.")
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileForm(instance=request.user.profile)
+    return render(request, 'blog/profile.html', {'u_form': u_form, 'p_form': p_form})
