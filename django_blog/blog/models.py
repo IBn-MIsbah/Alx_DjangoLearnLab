a516a24cc694f8cc57a4a10e783aa0a73e229bca
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-08-16 23:05:30 +0300

    feat(auth): add custom Profile model linked to User

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -9,3 +9,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
