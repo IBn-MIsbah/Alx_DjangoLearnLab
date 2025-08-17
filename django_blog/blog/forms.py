c60100dea8d6734934f132078d743d6f45070021
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-08-17 16:49:31 +0300

    Update PostForm to support adding/editing tags via comma-separated input

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Profile, Post, Comment
+from .models import Profile, Post, Comment, Tag
 
 class UserRegisterationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -33,12 +33,31 @@ class ProfileForm(forms.ModelForm):
         ]
 
 class PostForm(forms.ModelForm):
+    tags = forms.CharField(required=False, help_text='Comma-separated tags')
+
     class Meta:
         model = Post
         fields = [
             'title',
-            'content'
+            'content',
+            'tags'
         ]
+    def save(self, commit = True):
+        instance = super().save(commit=False)
+        if commit:
+            instance.save()
+        
+        tag_names = self.cleaned_data['tags'].split(',')
+        tag_objs = []
+
+        for name in tag_names:
+            name = name.strip()
+            if name:
+                tag_objs, created = Tag.objects.get_or_create(name=name)
+                tag_objs.append(tag_objs)
+        instance.tags.set(tag_objs)
+        return instance
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
