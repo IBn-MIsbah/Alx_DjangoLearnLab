67383b5d86f831cb29ea9dfca90eaa74961b7545
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-08-17 10:42:00 +0300

    feat: add Comment model with relations to Post and User

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.urls import reverse
+from .models import Post
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -22,3 +23,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s profile"
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Commented by {self.author} on {self.post}"
