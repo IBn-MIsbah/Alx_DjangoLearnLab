077b2773e1e023990ffb013e1e05baa737e60653
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-08-17 17:18:59 +0300

    feat: add django-taggit for post tagging
    - Installed and configured django-taggit
    - Added "taggit" to INSTALLED_APPS
    - Updated Post model to use TaggableManager for tags
    - Ran initial migrations for taggit

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
@@ -15,7 +16,7 @@ class Post(models.Model):
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post")
-    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
