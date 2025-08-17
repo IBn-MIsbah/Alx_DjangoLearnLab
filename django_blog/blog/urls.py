61fd04cb3c992cfefd7fe8aee7e5fbf6e56b9d76
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-08-17 17:01:08 +0300

    Add URL patterns for search and filtering posts by tag

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -19,4 +19,6 @@ urlpatterns = [
     path("post/<int:pk>/comments/new/", CommentCreateView.as_view(), name="add-comment"),
     path("comment/<int:pk>/update/", views.CommentUpdateView.as_view(), name="comment_edit"),
     path("comment/<int:pk>/delete/", views.CommentDeleteView.as_view(), name="comment_delete"),
+    path('search/', views.search_posts, name='search_posts'),
+    path('tags/<str:tag_name>/', views.posts_by_tag, name='posts_by_tag'),
 ]
