ad8b3d48618c1b12fe304baa902a6201dedc05be
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-08-24 11:14:10 +0300

    feat(accounts): implement follow and unfollow API endpoints

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, permission_classes
 from django.contrib.auth import authenticate
 from django.contrib.auth import get_user_model
 
@@ -39,3 +40,14 @@ class ProfileView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+@api_view(['POST'])
+@permission_classes([permissions.IsAuthenticated])
+def follow_user(request, user_id):
+    try:
+        target_user = User.objects.get(id=user_id)
+    except User.DoesNotExist:
+        return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+    
+    request.user.followinf.remove(target_user)
+    return Response({"message": f"You unfollowed {target_user.username}"}, status=200)
