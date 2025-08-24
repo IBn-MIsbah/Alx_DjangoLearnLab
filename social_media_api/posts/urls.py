a26e6c3a0b894e9e157af2f72acac2c80e71a3b2
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-08-24 15:08:36 +0300

    chore(posts): add URL routes for like and unlike endpoints

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet, feed
+from .views import PostViewSet, CommentViewSet, feed, LikePostView, UnlikePostView
 
 router = DefaultRouter()
 router.register(r"posts", PostViewSet)
@@ -8,5 +8,7 @@ router.register(r"comments", CommentViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("feed/", feed, name="feed")
+    path("feed/", feed, name="feed"),
+     path("<int:pk>/like/", LikePostView.as_view(), name="like-post"),
+    path("<int:pk>/unlike/", UnlikePostView.as_view(), name="unlike-post"),
 ]
