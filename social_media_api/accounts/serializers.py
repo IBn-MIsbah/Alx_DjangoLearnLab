3b03e5d3b61a6f580bc903637d1a2f1168c8bd84
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-08-24 10:11:21 +0300

    feat(auth): add user registration & login serializers with token support
    - Added RegisterSerializer with password confirmation and token creation
    - Added LoginSerializer with username/password validation using authenticate()
    - Integrated get_user_model().objects.create_user for secure user creation
    - Automatically generates auth token for new users

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
+from rest_framework.authtoken.models import Token
+
 
 User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     # Explicitly declare password fields
-    password = serializers.CharField(write_only=True, required=True)
-    password2 = serializers.CharField(write_only=True, required=True)
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+    password2 = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
 
     class Meta:
         model = User
@@ -21,4 +23,16 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         validated_data.pop('password2')
         # Use create_user so password is hashed
         user = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
         return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True, style={'input_type': 'password'})
+
+    def validate(self, data):
+        user = authenticate(username=data['username'], password=data['password'])
+        if not user:
+            raise serializers.ValidationError('Invalid login credentials')
+        data['user'] = user
+        return data
